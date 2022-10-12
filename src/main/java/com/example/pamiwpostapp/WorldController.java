3ce5c7f8c554be7b70807cc9aebac99c9b646f1a
mciekn <macni.macni@gmail.com>
2022-10-12 09:29:35 +0200

    Initial commit

---
+++ src/main/java/com/example/pamiwpostapp/WorldController.java
@@ -0,0 +1,39 @@
+package com.example.pamiwpostapp;
+
+import lombok.Value;
+import org.springframework.web.bind.annotation.*;
+
+@Value
+class Package{
+    String id;
+}
+
+@RestController
+@RequestMapping("package")
+public class WorldController {
+
+    private Package aPackage = new Package("Hello world");
+
+    @GetMapping
+    public Package getPackage(){
+        return(aPackage);
+    }
+
+    @PostMapping
+    public Package setPackage(@RequestBody String newWord){
+        aPackage = new Package(newWord);
+        return aPackage;
+    }
+
+    @PutMapping
+    public Package putPackage(@RequestBody String newWord){
+        //aPackage = new Package(newWord);
+        return aPackage;
+    }
+
+    @DeleteMapping
+    public Package deletePackage(@RequestBody String newWord){
+        //word = new Word(newWord);
+        return aPackage;
+    }
+}
---
+++ src/test/java/com/example/pamiwpostapp/PaMiWPostAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pamiwpostapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaMiWPostAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

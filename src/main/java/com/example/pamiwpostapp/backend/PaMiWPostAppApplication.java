8dc9e2847efa421cc8f46404e8bdd6d3ffc8a544
mciekn <macni.macni@gmail.com>
2022-10-18 19:14:18 +0200

    Refactor code, improve CRUD

--- src/main/java/com/example/pamiwpostapp/backend/PaMiWPostAppApplication.java
+++ src/main/java/com/example/pamiwpostapp/backend/PaMiWPostAppApplication.java
@@ -1,4 +1,4 @@
-package com.example.pamiwpostapp;
+package com.example.pamiwpostapp.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
